107f4eadf8cd9298c3265de41d4da77d457822cb
Corentin Régent <corentin.regent@gmail.com>
2024-07-31 07:30:28 +0000

    Randomly initialize the grid

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,15 @@
 %include "src/constants.asm"
+%include "src/random.asm"
 %include "src/utils.asm"
 
 SECTION .bss
-cols              resq 1
-rows              resq 1
-bytes_to_allocate resq 1
-grids_pointer     resq 1
+cols                 resq 1
+rows                 resq 1
+grid_size            resq 1
+bytes_per_grid       resq 1
+
+current_grid_pointer resq 1
+next_grid_pointer    resq 1
 
 SECTION .rodata
 msg_invalid_usage db "Invalid usage. Expected `./main [cols] [rows]`", NULL
@@ -14,78 +18,112 @@ SECTION .text
 global _start
 
 _start:
-    call .parseColsAndRows
-    call .computeBytesToAllocate
-    call .allocateGrids
-    call .deallocateGrids
+    call parseColsAndRows
+    call computeGridConstants
+    call allocateGrids
+    call initializeGrid
+    call deallocateGrids
     jmp exitOk
 
 ; void parseColsAndRows()
 ; Parses the command line arguments and loads the `cols` and `rows`
 ; variables, as 64-bit unsigned integers
-.parseColsAndRows:
-    pop rdx                       ; address of the calling function
-    pop rcx                       ; argc
-    cmp rcx, 3                    ; 3 arguments: program_name cols rows
-    jne .errInvalidUsage
-    add rsp, 8                    ; skip program_name
+parseColsAndRows:
+    pop rdx                              ; address of the calling function
+    pop rcx                              ; argc
+    cmp rcx, 3                           ; 3 arguments: program_name cols rows
+    jne errInvalidUsage
+    add rsp, 8                           ; skip program_name
 
     pop rdi
-    push rdx                      ; address of the calling function
+    push rdx                             ; address of the calling function
     call parseU64
-    jc .errInvalidUsage           ; argument is not a valid u64
+    jc errInvalidUsage                   ; argument is not a valid u64
     cmp rax, 0
-    je .errInvalidUsage
+    je errInvalidUsage
     mov [cols], rax
 
-    pop rdx                       ; address of the calling function
+    pop rdx                              ; address of the calling function
     pop rdi
     push rdx
     call parseU64
-    jc .errInvalidUsage           ; argument is not a valid u64
+    jc errInvalidUsage                   ; argument is not a valid u64
     cmp rax, 0
-    je .errInvalidUsage
+    je errInvalidUsage
     mov [rows], rax
     ret
 
-; void computeBytesToAllocate()
-; Computes the number of bytes to allocate in order to
-; have one byte per cell, for two grids (current and next state).
-; Updates the corresponding variable in the bss segment.
-.computeBytesToAllocate:
-    mov rax, [cols]
-    mov rcx, [rows]
-    imul rax, rcx
-    ; jc .errInvalidUsage                 ; u64 overflow
-    shl rax, 1                          ; twice as much memory, for two grids
-    ; jc .errInvalidUsage                 ; u64 overflow
-    mov [bytes_to_allocate], rax
-    ret
-
-.errInvalidUsage:
+errInvalidUsage:
     mov rdi, msg_invalid_usage
     jmp exitError
 
+; void computeGridConstants()
+; Computed constants related to the grid size
+; and updates the corresponding variables in the bss segment.
+computeGridConstants:
+    mov rax, [cols]
+    imul rax, [rows]
+    jc errInvalidUsage                   ; u64 overflow
+    mov [grid_size], rax
+    mov [bytes_per_grid], rax
+    ret
+
 ; void allocateGrids()
-; Allocates `bytes_to_allocate` bytes of memory
-; and makes the `grids_pointer` variable point to this area
-.allocateGrids:
+; Allocates the memory needed for the two grids
+; and updates the corresponding pointers in the bss segment.
+allocateGrids:
+    call .allocateGrid
+    mov [current_grid_pointer], rax
+    call .allocateGrid
+    mov [next_grid_pointer], rax
+    ret
+
+; void allocateGrid()
+; Allocates `bytes_per_grid` bytes of memory
+; Returns:
+;   rax: The pointer to this memory space
+.allocateGrid:
     mov rax, SYS_MMAP
-    xor rdi, rdi                        ; address will be chosen by the OS
-    mov rsi, [bytes_to_allocate]
+    xor rdi, rdi                         ; address will be chosen by the OS
+    mov rsi, [bytes_per_grid]
     mov rdx, PROT_READ_AND_WRITE
     mov r10, MAP_PRIVATE_AND_ANONYMOUS
-    mov r8,  -1                         ; not backed by a file descriptor
-    xor r9,  r9                         ; no offset
+    mov r8, -1                           ; no file descriptor
+    xor r9, r9                           ; no offset
     syscall
-    mov [grids_pointer], rax
+    ret
+
+; void initializeGrid()
+; Randomly generates the initial state of the grid
+initializeGrid:
+    call initRandomGenerator
+    mov rcx, [current_grid_pointer]
+    mov rdx, rcx
+    add rdx, [grid_size]                 ; address where we are no longer on the grid
+.loopOverCells:
+    call randomBoolean
+    mov [rcx], al                        ; set the value of the cell
+    inc rcx                              ; point to next cell
+    cmp rcx, rdx                         ; check for the end of the grid
+    jne .loopOverCells
+
     ret
 
 ; void deallocateGrids()
 ; Deallocates the memory that was allocated for the grids
-.deallocateGrids:
+deallocateGrids:
+    mov rdi, [current_grid_pointer]
+    call .deallocateGrid
+    mov rdi, [next_grid_pointer]
+    call .deallocateGrid
+    ret
+
+; void deallocateGrid()
+; Deallocates the memory that was allocated for the given grid
+; Arguments:
+;   rdi: The pointer to the grid to deallocate
+.deallocateGrid:
     mov rax, SYS_MUNMAP
-    mov rdi, [grids_pointer]
-    mov rsi, [bytes_to_allocate]
+    mov rsi, [bytes_per_grid]
     syscall
     ret
---
+++ src/random.asm
@@ -0,0 +1,34 @@
+SECTION .bss
+lcg_current_value              resq 1
+
+SECTION .text
+
+; void initRandomGenerator()
+; Initializes the Linear Congruential Generator
+; with a seed based on the CPU clock cycle count
+initRandomGenerator:
+    rdtsc                         ; load lower-order 32 bits of CPU cycle count into eax register
+    mov [lcg_current_value], eax
+    ret
+
+; char randomBoolean()
+; Returns:
+;   al: A pseudo-randomly generated boolean
+randomBoolean:
+    call random64bits
+    shr rax, 63                   ; keep the most significant bit
+    ret
+
+; void *random64bits()
+; Returns:
+;   rax: A pseudo-randomly generated 64-bit value
+random64bits:
+    ; lcg_current_value = (lcg_current_value*LCG_MULTIPLIER + LCG_INCREMENT) % (1<<64)
+    ;                   =  lcg_current_value*LCG_MULTIPLIER + LCG_INCREMENT
+    ; due to the carry values being lost beyond the 64-bit limit
+    ; Note that NASM issues a `[-w+number-overflow]` warning, but this overflow is intentional
+    mov rax, [lcg_current_value]
+    imul rax, LCG_MULTIPLIER
+    add rax, LCG_INCREMENT
+    mov [lcg_current_value], rax
+    ret
